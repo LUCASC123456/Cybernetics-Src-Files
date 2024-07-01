d3f5856be1802d745284f857c8e53544969d88ad
LUCASC123456 <lcox@students.mags.school.nz>
2024-07-01 21:05:00 +1200

    got health bar working

--- Cybernetics/scenes/main.gd
+++ Cybernetics/scenes/main.gd
@@ -3,18 +3,23 @@ extends Node
 signal enemy_killed
 
 var max_enemies : int
+var health : int
 var enemies_left : int
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	max_enemies = 10
+	health = 100
 	enemies_left = max_enemies
 	$UI/EnemyCounter/EnemiesLabel.text = "Enemies Left: " + str(enemies_left)
+	$UI/HealthBar.value = health
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
 
 func _on_enemy_spawner_hit_p():
+	health -= 10
+	$UI/HealthBar.value = health
 	pass
 
 func _on_enemy_killed():
