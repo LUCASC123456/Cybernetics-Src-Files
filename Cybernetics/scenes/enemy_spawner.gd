3dbe0def56b1c0572ea2b70d9de9a0a0a5c50bdd
LUCASC123456 <lcox@students.mags.school.nz>
2024-08-14 17:50:43 +1200

    added more levels/rooms with more enemies in them

--- Cybernetics/scenes/enemy_spawner.gd
+++ Cybernetics/scenes/enemy_spawner.gd
@@ -26,16 +26,14 @@ func _ready():
 			spawn_points_level_4.append(i)
 
 func _on_timer_timeout():
-	#check how many enemies have already been created
 	var enemies_level_1 = get_tree().get_nodes_in_group("enemies_level_1")
 	var enemies_level_2 = get_tree().get_nodes_in_group("enemies_level_2")
 	var enemies_level_3 = get_tree().get_nodes_in_group("enemies_level_3")
 	var enemies_level_4 = get_tree().get_nodes_in_group("enemies_level_4")
 	
 	if enemies_level_1.size() < get_parent().enemy_level_count[0]:
-		#pick a random spawnpoint
 		var spawn_level_1 = spawn_points_level_1[randi() % spawn_points_level_1.size()]
-		#spawn enemy
+		
 		var mafia_enforcer_level_1 = mafia_enforcer_scene.instantiate()
 		mafia_enforcer_level_1.position = spawn_level_1.position
 		mafia_enforcer_level_1.hit_player.connect(hit)
@@ -43,9 +41,8 @@ func _on_timer_timeout():
 		mafia_enforcer_level_1.add_to_group("enemies_level_1")
 		
 	if enemies_level_2.size() < get_parent().enemy_level_count[1]:
-		#pick a random spawnpoint
 		var spawn_level_2 = spawn_points_level_2[randi() % spawn_points_level_2.size()]
-		#spawn enemy
+		
 		var mafia_enforcer_level_2 = mafia_enforcer_scene.instantiate()
 		mafia_enforcer_level_2.position = spawn_level_2.position
 		mafia_enforcer_level_2.hit_player.connect(hit)
@@ -53,9 +50,8 @@ func _on_timer_timeout():
 		mafia_enforcer_level_2.add_to_group("enemies_level_2")
 		
 	if enemies_level_3.size() < get_parent().enemy_level_count[2]:
-		#pick a random spawnpoint
 		var spawn_level_3 = spawn_points_level_3[randi() % spawn_points_level_3.size()]
-		#spawn enemy
+		
 		var mafia_enforcer_level_3 = mafia_enforcer_scene.instantiate()
 		mafia_enforcer_level_3.position = spawn_level_3.position
 		mafia_enforcer_level_3.hit_player.connect(hit)
@@ -63,9 +59,8 @@ func _on_timer_timeout():
 		mafia_enforcer_level_3.add_to_group("enemies_level_1")
 	
 	if enemies_level_4.size() < get_parent().enemy_level_count[3]:
-		#pick a random spawnpoint
 		var spawn_level_4 = spawn_points_level_4[randi() % spawn_points_level_4.size()]
-		#spawn enemy
+		
 		var mafia_enforcer_level_4 = mafia_enforcer_scene.instantiate()
 		mafia_enforcer_level_4.position = spawn_level_4.position
 		mafia_enforcer_level_4.hit_player.connect(hit)
